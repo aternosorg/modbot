4fc1091bfb47717e068effcee96f8a28e6407b33
Mc88Donalds <Mc88Donalds@users.noreply.github.com>
2020-03-08 16:50:49 +0100

    grouped requires

--- bot.js
+++ bot.js
@@ -1,11 +1,11 @@
 const Discord = require('discord.js');
+const mysql = require('mysql');
 const config = require('./config');
 
 const bot = new Discord.Client();
 bot.login(config.auth_token);
 
 //connect to mysql db
-const mysql = require('mysql');
 const database = mysql.createConnection(config.db);
 database.connect(function(err) {
   if (err) throw err;
