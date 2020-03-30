bb5df739a0e03198df2b4a1cff04b6ce83dd10bc
Mc88Donalds <Mc88Donalds@users.noreply.github.com>
2020-03-30 18:52:06 +0200

    setting up channels table and loading channel modes from DB, prefix ignores capitalization

--- bot.js
+++ bot.js
@@ -5,22 +5,35 @@ const config = require('./config');
 const bot = new Discord.Client();
 bot.login(config.auth_token);
 
+var channels = new Discord.Collection();
+
 //connect to mysql db
 const database = mysql.createConnection(config.db);
 database.connect(function(err) {
   if (err) throw err;
   console.log("Connected!");
+  database.query("CREATE TABLE IF NOT EXISTS `channels` (`id` VARCHAR(20) NOT NULL, `mode` TINYINT NOT NULL, PRIMARY KEY (`id`), UNIQUE INDEX `id_UNIQUE` (`id` ASC) VISIBLE)");
+  // id => discord channel snowflake
+  // mode => 1 = reuqire ips, 2 = forbid ips
+
+  //load channels
+  database.query("SELECT * FROM channels",function(err, result){
+    if(err) throw err;
+    result.forEach( row => {
+      channels.set(row.id, row.mode);
+    });
+  });
 });
 
 //bot commands
 bot.on('message', async  (message) => {
   if(!message.guild || message.author.bot) return;
-  if(!message.content.startsWith(config.prefix)) return;
+  if(!message.content.toLowerCase().startsWith(config.prefix.toLowerCase())) return;
 
   const args = message.content.split(/\s+/g);
-  const command = args.shift().slice(cfg.prefix.length).toLowerCase();
+  const command = args.shift().slice(config.prefix.length).toLowerCase();
 
   switch(command){
-
+    
   }
-})
+});
