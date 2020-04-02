85f624c51c7631f77685152ca02b75f5905ebdcc
Mc88Donalds <Mc88Donalds@users.noreply.github.com>
2020-04-02 19:03:12 +0200

    formated the responses and mentioned the user

--- features/automod.js
+++ features/automod.js
@@ -9,7 +9,7 @@ exports.message = (message, channels) => {
   }
 
   if(mode === 1 && !message.content.includes('.aternos.me')){
-    message.channel.send('Your message to this channel must include a valid Aternos IP.')
+    message.channel.send(`**:no_entry: <@${message.author.id}> your message to this channel must include a valid Aternos IP! :no_entry:**`)
     .then(response =>
       response.delete({timeout: 5000})
     );
@@ -17,7 +17,7 @@ exports.message = (message, channels) => {
     return;
   }
   if(mode === 2 && message.content.includes('.aternos.me')){
-    message.channel.send('Dont advertise your server here!')
+    message.channel.send(`**:no_entry: <@${message.author.id}> don't advertise your server here! :no_entry:**`)
     .then(response =>
       response.delete({timeout: 5000})
     );
