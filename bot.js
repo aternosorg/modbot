e785243ac847fe93e535c8da20d06a1a009cd6ba
Mc88Donalds <Mc88Donalds@users.noreply.github.com>
2020-04-02 18:47:04 +0200

    added channel automod

--- bot.js
+++ bot.js
@@ -40,3 +40,32 @@ bot.on('message', async  (message) => {
       break;
   }
 });
+
+//ip automod
+bot.on('message', async  (message) => {
+  if(!message.guild || message.author.bot) return;
+  if(message.member.hasPermission('MANAGE_MESSAGES'))
+    return;
+
+  let mode = channels.get(message.channel.id);
+  if(!mode){
+    return;
+  }
+  
+  if(mode === 1 && !message.content.includes('.aternos.me')){
+    message.channel.send('Your message to this channel must include a valid Aternos IP.')
+    .then(response =>
+      response.delete({timeout: 5000})
+    );
+    message.delete();
+    return;
+  }
+  if(mode === 2 && message.content.includes('.aternos.me')){
+    message.channel.send('Dont advertise your server here!')
+    .then(response =>
+      response.delete({timeout: 5000})
+    );
+    message.delete();
+    return;
+  }
+});
