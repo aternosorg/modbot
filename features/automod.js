f5e0a96e8bb494e1c90b53f538aeb1b2a54af92e
Mc88Donalds <Mc88Donalds@users.noreply.github.com>
2020-04-03 21:47:36 +0200

    added error handling to message deletion

--- features/automod.js
+++ features/automod.js
@@ -11,17 +11,17 @@ exports.message = (message, channels) => {
   if(mode === 1 && !message.content.toLowerCase().includes('.aternos.me')){
     message.channel.send(`**:no_entry: <@${message.author.id}> your message to this channel must include a valid Aternos IP! :no_entry:**`)
     .then(response =>
-      response.delete({timeout: 5000})
+      response.delete({timeout: 5000}).catch(error => console.error("Failed to delete a Message! ",error))
     );
-    message.delete();
+    message.delete().catch(error => console.error("Failed to delete a Message! ",error));
     return;
   }
   if(mode === 2 && message.content.toLowerCase().includes('.aternos.me')){
     message.channel.send(`**:no_entry: <@${message.author.id}> don't advertise your server here! :no_entry:**`)
     .then(response =>
-      response.delete({timeout: 5000})
+      response.delete({timeout: 5000}).catch(error => console.error("Failed to delete a Message! ",error))
     );
-    message.delete();
+    message.delete().catch(error => console.error("Failed to delete a Message! ",error));
     return;
   }
 }
