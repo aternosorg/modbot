ae2e81da2f8d7581ce37fdf0d4178b24fa30eb14
Kurt Thiemann <info@kurtthiemann.com>
2020-04-30 20:10:24 +0200

    cleanup code, use async/await correctly

--- features/automod.js
+++ features/automod.js
@@ -1,27 +1,35 @@
-//removes messages with(out) IPs in specific channels
-exports.message = (message, channels) => {
-  if(!message.guild || message.author.bot || message.member.hasPermission('MANAGE_MESSAGES'))
-    return;
+const util = require('../lib/util');
 
-  if(channels.get(message.channel.id)&&channels.get(message.channel.id).mode){
-    let mode = channels.get(message.channel.id).mode;
-    if(mode === 1 && !message.content.toLowerCase().includes('.aternos.me')){
-      //delete non IP messages in IP only channels
-      message.channel.send(`**:no_entry: <@${message.author.id}> your message to this channel must include a valid Aternos IP! :no_entry:**`)
-      .then(response =>
-        response.delete({timeout: 5000}).catch(error => console.error("Failed to delete a Message! ",error))
-      );
-      message.delete().catch(error => console.error("Failed to delete a Message! ",error));
-      return;
+//removes messages with(out) IPs in specific channels
+exports.message = async (message, channels) => {
+    if (!message.guild || message.author.bot || message.member.hasPermission('MANAGE_MESSAGES'))
+        return;
+
+    if (channels.get(message.channel.id) && channels.get(message.channel.id).mode) {
+        let mode = channels.get(message.channel.id).mode;
+        if (mode === 1 && !message.content.toLowerCase().includes('.aternos.me')) {
+            //delete non IP messages in IP only channels
+            let response = await message.channel.send(`**:no_entry: <@${message.author.id}> your message to this channel must include a valid Aternos IP! :no_entry:**`);
+            try {
+                await util.retry(message.delete, message);
+                await util.retry(response.delete, response, [{timeout: 5000}]);
+            } catch (e) {
+                console.error('Failed to delete message', e);
+            }
+            return;
+        }
+        if (mode === 2 && message.content.toLowerCase().includes('.aternos.me')) {
+            //Delete IPs in no IP channels
+            let response = await message.channel.send(`**:no_entry: <@${message.author.id}> don't advertise your server here! :no_entry:**`);
+            try {
+                await util.retry(message.delete, message);
+                await util.retry(response.delete, response, [{timeout: 5000}]);
+            } catch (e) {
+                console.error('Failed to delete message', e);
+            }
+        }
     }
-    if(mode === 2 && message.content.toLowerCase().includes('.aternos.me')){
-      //Delete IPs in no IP channels
-      message.channel.send(`**:no_entry: <@${message.author.id}> don't advertise your server here! :no_entry:**`)
-      .then(response =>
-        response.delete({timeout: 5000}).catch(error => console.error("Failed to delete a Message! ",error))
-      );
-      message.delete().catch(error => console.error("Failed to delete a Message! ",error));
-      return;
-    }
-  }
+}
+
+exports.init = async (database, channels, bot) => {
 }
--- features/cooldown.js
+++ features/cooldown.js
@@ -1,71 +1,68 @@
+const util = require('../lib/util');
+
 //cooldown automod
-exports.message = (message, channels, database) => {
-  if(!message.guild || message.author.bot || message.member.hasPermission('MANAGE_MESSAGES'))
-    return;
+exports.message = async (message, channels, database) => {
+    if (!message.guild || message.author.bot || message.member.hasPermission('MANAGE_MESSAGES'))
+        return;
 
-  if(channels.get(message.channel.id)&&channels.get(message.channel.id).cooldown){
-    let cooldown = channels.get(message.channel.id).cooldown;
-    if(message.content.includes('.aternos.me')) {
+    if (channels.get(message.channel.id) && channels.get(message.channel.id).cooldown) {
+        let cooldown = channels.get(message.channel.id).cooldown;
+        if (message.content.includes('.aternos.me')) {
 
-      //get all IPs
-      let words = message.content.replace(/[^0-9a-z .]/gi, ' ').split(' ');
-      let ips = words.filter(word => word.includes('.aternos.me'));
+            //get all IPs
+            let words = message.content.replace(/[^0-9a-z .]/gi, ' ').split(' ');
+            let ips = words.filter(word => word.includes('.aternos.me'));
 
-      ips.forEach( ip => {
-        let server = ip.replace('.aternos.me', '');
-        database.query('SELECT * FROM servers WHERE channelid = ? AND ip = ?', [message.channel.id, server], function(err, result) {
-          if(result[0]){
-            let data = result[0];
+            for (let ip of ips) {
+                let server = ip.replace('.aternos.me', '');
+                let data = await database.query('SELECT * FROM servers WHERE channelid = ? AND ip = ? ORDER BY `timestamp` DESC;', [message.channel.id, server]);
 
-            //seconds until ip is allowed to be advertised again
-            let difference = parseInt(data.timestamp,10) + cooldown - Math.floor(Date.now()/1000);
-            if (difference>59){
-              //remaing timer
-              let remaining = '';
-              if (Math.floor(difference/(60*60*24))!=0) {
-                remaining += Math.floor(difference/(60*60*24)) + 'd ';
-              }
-              if (Math.floor(difference/(60*60))!=0) {
-                remaining += Math.floor(difference%(60*60*24)/(60*60)) + 'h ';
-              }
-              if (Math.floor(difference/60)!=0) {
-                remaining += Math.floor(difference%(60*60)/60) + 'm ';
-              }
+                if (!data) {
+                    await database.query('INSERT INTO servers (channelid, ip, `timestamp`) VALUES (?,?,?)', [message.channel.id, server, (Math.floor(Date.now() / 1000))]);
+                    continue;
+                }
 
-              message.channel.send(`You can advertise again in ${remaining}!`)
-              .then(response =>
-                response.delete({timeout: 5000}).catch(error => console.error("Failed to delete a Message! ",error))
-              );
-              message.delete().catch(error => console.error("Failed to delete a Message! ",error));
-              return;
+                let difference = parseInt(data.timestamp, 10) + cooldown - Math.floor(Date.now() / 1000);
+                if (difference <= 60) {
+                    await database.query('UPDATE servers SET `timestamp` = ? WHERE channelid = ? AND ip = ?', [(Math.floor(Date.now() / 1000)), message.channel.id, server]);
+                    continue;
+                }
+
+                let remaining = '';
+                if (Math.floor(difference / (60 * 60 * 24)) !== 0) {
+                    remaining += Math.floor(difference / (60 * 60 * 24)) + 'd ';
+                }
+                if (Math.floor(difference / (60 * 60)) !== 0) {
+                    remaining += Math.floor(difference % (60 * 60 * 24) / (60 * 60)) + 'h ';
+                }
+                if (Math.floor(difference / 60) !== 0) {
+                    remaining += Math.floor(difference % (60 * 60) / 60) + 'm ';
+                }
+
+                let response = await message.channel.send(`You can advertise again in ${remaining}!`);
+                try {
+                    await util.retry(message.delete, message);
+                    await util.retry(response.delete, response, [{timeout: 5000}]);
+                } catch (e) {
+                    console.error('Failed to delete message', e);
+                }
+                break;
             }
-            else {
-              //Update time in database
-              database.query('UPDATE servers SET timestamp = ? WHERE channelid = ? AND ip = ?',[(Math.floor(Date.now()/1000)), message.channel.id, server]);
-            }
-          }
-          else {
-            //add ip and time to database
-            database.query('INSERT INTO servers (channelid, ip, timestamp) VALUES (?,?,?)', [message.channel.id, server, (Math.floor(Date.now()/1000))])
-          }
-        });
-      });
+        }
     }
-  }
 }
 
-//delete unneccessary servers
-exports.clean = (database, channels, bot) => {
-  channels.forEach(channel => {
-    //IPs with a smaller timestamp can already be advertised again
-    let relevant = Math.floor(Date.now()/1000) - channel.cooldown;
-    database.query('DELETE FROM servers WHERE channelid = ? AND timestamp <= ?', [channel.id, relevant])
-  });
-  //stats on servers size
-  database.query('SELECT * FROM servers', function(err,result) {
-    date = new Date();
-    console.log(`[${date.getUTCHours()}:${date.getUTCMinutes()}] There are currently ${result.length} servers in the Database!`);
-    bot.user.setActivity(`${result.length} servers`, {type: 'WATCHING'});
-    setTimeout(() => {bot.user.setActivity('https://git.io/Jvhfg', {type: 'WATCHING'});}, 900000);
-  })
+exports.init = async (database, channels, bot) => {
+    await clean(database, channels, bot);
+}
+
+async function clean(database, channels, bot) {
+    for (let channel of channels) {
+        let relevant = Math.floor(Date.now() / 1000) - (isNaN(channel.cooldown) ? 0 : channel.cooldown);
+        await database.query("DELETE FROM servers WHERE channelid = ? AND timestamp <= ?", [channel.id, relevant]);
+    }
+    //stats on servers size
+    let result = await database.query("SELECT COUNT(*) as c FROM servers");
+    let date = new Date();
+    console.log(`[${date.getUTCHours()}:${date.getUTCMinutes()}] There are currently ${result['c']} servers in the Database!`);
 }
