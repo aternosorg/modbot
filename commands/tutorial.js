8803c02be1e1214d6cb125bbea32a4143007dde6
Julian Vennen <julianvennen@gmail.com>
2020-06-05 21:22:37 +0200

    youtube video caching

--- commands/tutorial.js
+++ commands/tutorial.js
@@ -1,5 +1,6 @@
 const config = require('../config.json');
 const {google} = require('googleapis');
+let videos;
 
 exports.command = async (message, args, channels, database) => {
 
@@ -10,15 +11,22 @@ exports.command = async (message, args, channels, database) => {
     return ;
   }
 
-  var service = google.youtube('v3');
-  let response = await service.playlistItems.list({
-    auth: config.googleapikey,
-    part: 'snippet,contentDetails,id',
-    playlistId: "PLHn1eAE9tVwzD2pnhzfvCj9h-e06MfH2N",
-    maxResults: 100
-  });
+  if(!videos){
+    console.log('Refreshing video cache...');
+    let service = google.youtube('v3');
+    let response = await service.playlistItems.list({
+      auth: config.googleapikey,
+      part: 'snippet,contentDetails,id',
+      playlistId: "PLHn1eAE9tVwzD2pnhzfvCj9h-e06MfH2N",
+      maxResults: 100
+    });
+    videos = response.data.items;
+    setTimeout(() => {
+      videos = null;
+      console.log('Cleared video cache');
+    }, 10*60*1000);
+  }
 
-  var videos = response.data.items;
   for(video of videos){
     if(video.snippet.title.toLowerCase().includes(query)){
       await message.channel.send('https://youtu.be/'+video.snippet.resourceId.videoId);
