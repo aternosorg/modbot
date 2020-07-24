a3e766586a7d8fa23b846d7a6a3844c5c965999a
Julian <julianvennen@gmail.com>
2020-07-24 18:38:06 +0200

    role check & moderator logging

--- commands/ban.js
+++ commands/ban.js
@@ -11,20 +11,27 @@ exports.command = async (message, args, database, bot) => {
   let user = await bot.users.fetch(userId);
 
   if (!user) {
+    message.react('🛑');
     message.channel.send("User not found!");
     return;
   }
 
   //highest role check
+  if(message.guild.members.resolve(user)) {
+    if(message.member.roles.highest.comparPositionTo(message.members.resolve(user).roles.highest) <= 0)
+      message.react('🛑');
+      message.channel.send("You dont have the Permission to ban that Member!")
+      return;
+  }
 
   let duration = util.timeToSec(args.join(' '));
+  let reason = (args.join(' ') || 'No reason provided.')
+  let now = Math.floor(Date.now()/1000);
   if (duration) {
     let time = util.secToTime(duration);
     while (util.isTime(args[0]))
       args.shift();
 
-    let reason = (args.join(' ') || 'No reason provided.')
-    let now = Math.floor(Date.now()/1000);
     let endsAt = now + duration;
 
     message.guild.members.ban(userId, {days: 7, reason: `duaration: ${time} reason:` + reason});
@@ -32,28 +39,25 @@ exports.command = async (message, args, database, bot) => {
     //check user was already banned
     let ban = await database.query("SELECT action FROM moderations WHERE guildid = ? AND userid = ? AND action = 'ban'", [message.guild.id, userId])
     if (ban) {
-      database.query("UPDATE moderations SET lastChanged = ?, value = ?, reason = ? WHERE guildid = ? AND userid = ? AND action = 'ban'",[now, endsAt, reason, message.guild.id, userId]);
+      database.query("UPDATE moderations SET lastChanged = ?, value = ?, reason = ?, moderator = ? WHERE guildid = ? AND userid = ? AND action = 'ban'",[now, endsAt, reason, message.author.id, message.guild.id, userId]);
     }
     else {
-      database.query("INSERT INTO moderations (guildid, userid, action, lastChanged, value, reason) VALUES (?,?,'ban',?,?,?)",[message.guild.id, userId, now, endsAt, reason]);
+      database.query("INSERT INTO moderations (guildid, userid, action, lastChanged, value, reason, moderator) VALUES (?,?,'ban',?,?,?,?)",[message.guild.id, userId, now, endsAt, reason, message.author.id]);
     }
 
     util.log(message, `Banned \`${user.username}#${user.discriminator}\` for ${time}: ${reason}`);
-    message.channel.send(`Banned \`${user.username}#${user.discriminator}\` for ${time}: ${reason}`);
+    message.channel.send(`${message.author.username} banned \`${user.username}#${user.discriminator}\` for ${time}: ${reason}`);
   }
   else {
-    let reason = (args.join(' ') || 'No reason provided.')
-    let now = Math.floor(Date.now()/1000);
-
     message.guild.members.ban(userId, {days: 7, reason: reason});
 
     //check user was already banned
     let ban = await database.query("SELECT action FROM moderations WHERE guildid = ? AND userid = ? AND action = 'ban'", [message.guild.id, userId])
     if (ban) {
-      database.query("UPDATE moderations SET value = 0, tocheck = false, lastChanged = ?, reason = ? WHERE guildid = ? AND userid = ? AND action = 'ban'",[now, reason, message.guild.id, userId]);
+      database.query("UPDATE moderations SET value = 0, tocheck = false, lastChanged = ?, reason = ?, moderator = ? WHERE guildid = ? AND userid = ? AND action = 'ban'",[now, reason, message.author.id, message.guild.id, userId]);
     }
     else {
-      database.query("INSERT INTO moderations (action, value, tocheck, guildid, userid, lastChanged, reason) VALUES ('ban',0,false,?,?,?,?)",[message.guild.id, userId, now, reason]);
+      database.query("INSERT INTO moderations (action, value, tocheck, guildid, userid, lastChanged, reason, moderator) VALUES ('ban',0,false,?,?,?,?,?)",[message.guild.id, userId, now, reason,message.author.id]);
     }
 
     util.log(message, `Banned \`${user.username}#${user.discriminator}\`: ${reason}`);
