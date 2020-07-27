cc371e05141cac85e9bdc10141c6c7e1d1df7cbc
Julian <julianvennen@gmail.com>
2020-07-28 00:14:37 +0200

    await v3

--- checks/tempban.js
+++ checks/tempban.js
@@ -9,9 +9,9 @@ exports.check = async (database, bot) => {
       let user = await bot.users.fetch(result.userid);
       let insert = await database.queryAll("INSERT INTO moderations (guildid, userid, action, created, reason, active) VALUES (?,?,?,?,?,?)",[result.guildid,result.userid,'unban',Math.floor(Date.now()/1000),"Temporary ban completed!", false]);
 
-      util.log(result.guildid, `\`[${insert.insertId}]\` Unbanned \`${user.username}#${user.discriminator}\`: Temporary ban completed!`);
+      await util.log(result.guildid, `\`[${insert.insertId}]\` Unbanned \`${user.username}#${user.discriminator}\`: Temporary ban completed!`);
 
-      database.query("UPDATE moderations SET active = FALSE WHERE action = 'ban' AND userid = ? AND guildid = ?",[result.userid,result.guildid]);
+      await database.query("UPDATE moderations SET active = FALSE WHERE action = 'ban' AND userid = ? AND guildid = ?",[result.userid,result.guildid]);
     } catch (e) {
       console.error(`Couldn't unban user ${result.userid} in ${result.guildid}`, e);
     }
--- checks/tempmute.js
+++ checks/tempmute.js
@@ -7,16 +7,16 @@ exports.check = async (database, bot) => {
       if (bot.guilds.resolve(result.guildid).members.resolve(result.userid)) {
         let member = bot.guilds.resolve(result.guildid).members.resolve(result.userid);
         let guildConfig = util.getGuildConfig(result.guildid)
-        member.roles.remove([guildConfig.mutedRole], "Temporary mute completed!");
-        member.send(`You were unmuted in \`${message.guild.name}\`: Temporary mute completed!`);
+        await member.roles.remove([guildConfig.mutedRole], "Temporary mute completed!");
+        await member.send(`You were unmuted in \`${message.guild.name}\`: Temporary mute completed!`);
       }
 
       let user = await bot.users.fetch(result.userid);
       let insert = await database.queryAll("INSERT INTO moderations (guildid, userid, action, created, reason, active) VALUES (?,?,?,?,?,?)",[result.guildid,result.userid,'unban',Math.floor(Date.now()/1000),"Temporary ban completed!", false]);
 
-      util.log(result.guildid, `\`[${insert.insertId}]\` Unmuted \`${user.username}#${user.discriminator}\`: Temporary mute completed!`);
+      await util.log(result.guildid, `\`[${insert.insertId}]\` Unmuted \`${user.username}#${user.discriminator}\`: Temporary mute completed!`);
 
-      database.query("UPDATE moderations SET active = FALSE WHERE action = 'mute' AND userid = ? AND guildid = ?",[result.userid,result.guildid]);
+      await database.query("UPDATE moderations SET active = FALSE WHERE action = 'mute' AND userid = ? AND guildid = ?",[result.userid,result.guildid]);
     } catch (e) {
       console.error(`Couldn't unmute user ${result.userid} in ${result.guildid}`, e);
     }
