ee4a15070d63b608614c2b45376afe7f6d30c6c2
Julian <julianvennen@gmail.com>
2020-07-29 15:58:53 +0200

    add moderation entries to db => util

--- commands/ban.js
+++ commands/ban.js
@@ -15,7 +15,7 @@ exports.command = async (message, args, database, bot) => {
 
   let user;
   try {
-    user = await bot.users.fetch(util.userMentionToId(args.shift()));
+    user = await bot.users.fetch(userId);
   } catch (e) {
     await message.react(util.icons.error);
     await message.channel.send("Please provide a user (@Mention or ID)!");
@@ -45,25 +45,17 @@ exports.command = async (message, args, database, bot) => {
   let reason = args.join(' ') || 'No reason provided.';
   let now = Math.floor(Date.now()/1000);
 
-  let ban = await database.query("SELECT * FROM moderations WHERE active = TRUE AND guildid = ? AND userid = ? AND action = 'ban'", [message.guild.id, userId]);
-  //check user was already banned
-  if (ban) {
-    await database.query("UPDATE moderations SET active = FALSE WHERE guildid = ? AND userid = ? AND action = 'ban'", [message.guild.id, userId])
-  }
+  let id = await util.moderationDBAdd(message.guild.id, userId, 'ban', reason, duration, message.author.id);
 
   if (duration) {
     let time = util.secToTime(duration);
-    let endsAt = now + duration;
+    await message.guild.members.ban(userId, {days: 7, reason: `${message.author.username}#${message.author.discriminator} (${time}), Reason:` + reason});
 
     if (member) {
       await member.send(`You were banned from \`${message.guild.name}\` for ${time} | ${reason}`);
     }
-    await message.guild.members.ban(userId, {days: 7, reason: `${message.author.username}#${message.author.discriminator} (${time}), Reason:` + reason});
-
-    let insert = await database.queryAll("INSERT INTO moderations (guildid, userid, action, created, expireTime, reason, moderator) VALUES (?,?,?,?,?,?,?)",[message.guild.id, userId, 'ban', now, endsAt, reason, message.author.id]);
-
     await message.channel.send(`Banned \`${user.username}#${user.discriminator}\` for ${time} | ${reason}`);
-    await util.logMessage(message, `\`[${insert.insertId}]\` \`${message.author.username}#${message.author.discriminator}\` banned \`${user.username}#${user.discriminator}\` (ID: ${user.id})\nDuration: **${time}**\nReason: **${reason}**`);
+    await util.logMessage(message, `\`[${id}]\` \`${message.author.username}#${message.author.discriminator}\` banned \`${user.username}#${user.discriminator}\` (ID: ${user.id})\nDuration: **${time}**\nReason: **${reason}**`);
   }
   else {
     if (member) {
@@ -71,10 +63,8 @@ exports.command = async (message, args, database, bot) => {
     }
     await message.guild.members.ban(userId, {days: 7, reason: `${message.author.username}#${message.author.discriminator}, Reason: ` + reason});
 
-    let insert = await database.queryAll("INSERT INTO moderations (guildid, userid, action, created, reason, moderator) VALUES (?,?,?,?,?,?)",[message.guild.id, userId, 'ban', now, reason, message.author.id]);
-
     await message.channel.send(`Banned \`${user.username}#${user.discriminator}\` | ${reason}`);
-    await util.logMessage(message, `\`[${insert.insertId}]\` \`${message.author.username}#${message.author.discriminator}\` banned \`${user.username}#${user.discriminator}\` (ID: ${user.id})\nReason: **${reason}**`);
+    await util.logMessage(message, `\`[${id}]\` \`${message.author.username}#${message.author.discriminator}\` banned \`${user.username}#${user.discriminator}\` (ID: ${user.id})\nReason: **${reason}**`);
   }
 }
 
