16f25fbffce110842cda7612a0e75922b4ffed0f
Julian <julianvennen@gmail.com>
2020-07-31 14:45:44 +0200

    deactivate bans instead of deleting them

--- features/guildBanRemoved/deleteBan.js
+++ features/guildBanRemoved/deleteBan.js
@@ -3,7 +3,7 @@ const util = require('../../lib/util');
 exports.message = async (guild, user, database) => {
   let result = await database.query("SELECT * FROM moderations WHERE action = 'ban' AND active = TRUE AND userid = ? AND guildid = ?",[user.id,guild.id]);
   if (result) {
-    await database.query("DELETE * FROM moderations WHERE action = 'ban' AND active = TRUE AND userid = ? AND guildid = ?",[user.id,guild.id]);
+    await database.query("UPDATE moderations SET active = FALSE WHERE action = 'ban' AND active = TRUE AND userid = ? AND guildid = ?",[user.id,guild.id]);
     await util.logMessage(guild, `Deleted ban [${result.id}] because user was unbanned in this guild!`);
   }
 }
