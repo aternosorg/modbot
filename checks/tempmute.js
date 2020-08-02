f7133ee4ffc2859ee3247e4ee27c16b9870f16d1
Julian <julianvennen@gmail.com>
2020-08-02 14:25:18 +0200

    try catch tempmutes

--- checks/tempmute.js
+++ checks/tempmute.js
@@ -4,15 +4,17 @@ exports.check = async (database, bot) => {
   let results = await database.queryAll("SELECT * FROM moderations WHERE action = 'mute' AND active = TRUE AND expireTime IS NOT NULL AND expireTime <= ?", [Math.floor(Date.now()/1000)]);
   for (let result of results) {
     try {
-      if (bot.guilds.resolve(result.guildid).members.fetch(result.userid)) {
-        let member = bot.guilds.resolve(result.guildid).members.fetch(result.userid);
-        let guildConfig = await util.getGuildConfig(result.guildid)
-
-        if (member.roles.cache.get(guildConfig.mutedRole)) {
-          await member.roles.remove([guildConfig.mutedRole], "Temporary mute completed!");
-          await member.send(`You were unmuted in \`${bot.guilds.resolve(result.guildid).name}\`: Temporary mute completed!`);
+      let member
+      try {
+        member = bot.guilds.resolve(result.guildid).members.fetch(result.userid);
+        if (member) {
+          let guildConfig = await util.getGuildConfig(result.guildid);
+          if (member.roles.cache.get(guildConfig.mutedRole)) {
+            await member.roles.remove([guildConfig.mutedRole], "Temporary mute completed!");
+            await member.send(`You were unmuted in \`${bot.guilds.resolve(result.guildid).name}\`: Temporary mute completed!`);
+          }
         }
-      }
+      } catch (e) {}
 
       let user = await bot.users.fetch(result.userid);
       let insert = await database.queryAll("INSERT INTO moderations (guildid, userid, action, created, reason, active) VALUES (?,?,?,?,?,?)",[result.guildid,result.userid,'unban',Math.floor(Date.now()/1000),"Temporary ban completed!", false]);
