a7f3b825c2c1234aaa9048827b96fb9b124999a1
Chosen-Fate <55001472+Chosen-Fate@users.noreply.github.com>
2020-08-09 13:51:03 +0200

    Add help command file and set it up for bluewy

---
+++ commands/help.js
@@ -0,0 +1,11 @@
+const util = require('../lib/util.js');
+
+const command = {};
+
+command.command = async (message, args, database, bot) => {
+
+
+}
+command.names = ['help'];
+
+module.exports = command;
