27d657f435d888ccb4f86772ad709e90544c4892
Julian <julianvennen@gmail.com>
2020-08-10 23:20:14 +0200

    correct usage for ipcooldown command

--- commands/ipcooldown.js
+++ commands/ipcooldown.js
@@ -5,7 +5,7 @@ const command = {};
 
 command.description = 'Set a cooldown on ips in a channel';
 
-command.usage = 'example.zendesk.com|example|disabled';
+command.usage = '#channel|channelId duration';
 
 command.comment = 'If the cooldown has less then 60s remaining the message will not be deleted';
 
