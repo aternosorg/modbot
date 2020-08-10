9ca8189f3f3a598f5c47f523fd1a8cfad6ba8a72
Julian <julianvennen@gmail.com>
2020-08-10 21:24:35 +0200

    rewrite help command to show descriptions for single commands

--- commands/help.js
+++ commands/help.js
@@ -5,30 +5,35 @@ const fs = require('fs').promises;
 
 const command = {};
 
+command.description = 'List all commands or a description to a single command';
+
+command.usage = '<command>';
+
+command.names = ['help'];
+
 const commands = {};
 let commandList = '';
 
 (async () => {
   for (let file of await fs.readdir(`${__dirname}`)) {
-      let path = `${__dirname}/${file}`;
-      if (!file.endsWith('.js') || !(await fs.lstat(path)).isFile()) {
-          continue;
+    let path = `${__dirname}/${file}`;
+    if (!file.endsWith('.js') || !(await fs.lstat(path)).isFile()) {
+      continue;
+    }
+    try {
+      let cmd = require(path);
+      for (let name of cmd.names) {
+        commands[name] = cmd;
       }
-      try {
-          let cmd = require(path);
-          for (let name of cmd.names) {
-            commands[name] = cmd;
-          }
-          commandList += `${cmd.names[0]}, `;
-      } catch (e) {
-          console.error(`Failed to load command '${file}'`, e);
-      }
+      commandList += `${cmd.names[0]}, `;
+    } catch (e) {
+      console.error(`Failed to load command '${file}'`, e);
+    }
   }
   commandList = commandList.substring(0, commandList.length - 2);
 })();
 
-
-command.command = async (message, args, database, bot) => {
+command.execute = async (message, args, database, bot) => {
   const embed = new Discord.MessageEmbed()
   .setColor(util.color.green)
   .setFooter(`Command executed by ${message.author.username}`)
@@ -60,7 +65,5 @@ command.command = async (message, args, database, bot) => {
   message.channel.send(embed);
 
 };
-
-command.names = ['help'];
 
 module.exports = command;
