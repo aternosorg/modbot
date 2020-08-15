d291d37f575044649ac044793cfb1c0dfd411a86
Julian <julianvennen@gmail.com>
2020-08-15 15:50:54 +0200

    remove debugging

--- features/message/ipmod.js
+++ features/message/ipmod.js
@@ -2,8 +2,6 @@ const util = require('../../lib/util');
 
 //removes messages with(out) IPs in specific channels
 exports.event = async (database, message) => {
-  console.log(database);
-  console.log(message);
     if (!message.guild || message.author.bot || message.member.hasPermission('MANAGE_MESSAGES'))
         return;
 
