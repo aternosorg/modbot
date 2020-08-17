147b2a528983c3578adc776e15de3e6d8c15c535
Julian <julianvennen@gmail.com>
2020-08-17 19:15:22 +0200

    document ChannelConfig

--- util/ChannelConfig.js
+++ util/ChannelConfig.js
@@ -1,15 +1,30 @@
+const Discord = require('discord.js');
+
+/**
+ * class representing the config of a channel
+ */
+class ChannelConfig {
+
+    /**
+     * constructor - create a channel config
+     *
+     * @param  {Discord.Snowflake} id             channel id
+     * @param  {Object}           [json]          options
+     * @param  {Number}           [json.mode]     ip automod mode (0 => disabled, 1 => required, 2 => forbidden)
+     * @param  {Number}           [json.cooldown] ip cooldown in seconds
+     * @param  {Boolean}          [json.invites]  allow invites
+     * @return {channelConfig} the config of the channel
+     */
+
     constructor(id, json) {
-        //channel ID
         this.id = id;
 
         if (json) {
-          //channel Mode (automod)
           this.mode = json.mode;
-          //IP cooldown time
           this.cooldown = json.cooldown;
-          //allow invites
           this.invites = json.invites;
         }
     }
 }
 
+module.exports = ChannelConfig;
