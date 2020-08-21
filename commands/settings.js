3944c892d161ea3da564fb9e2bfac41cfb9081b7
Julian <julianvennen@gmail.com>
2020-08-21 19:01:50 +0200

    indent

--- commands/settings.js
+++ commands/settings.js
@@ -10,36 +10,36 @@ command.usage = '';
 command.names = ['settings','options'];
 
 command.execute = async (message, args, database, bot) => {
-    //Permission check
-    if (!message.member.hasPermission('MANAGE_GUILD')) {
-        await message.channel.send('You need the "Manage Server" permission to use this command.');
-        return;
-    }
+  //Permission check
+  if (!message.member.hasPermission('MANAGE_GUILD')) {
+    await message.channel.send('You need the "Manage Server" permission to use this command.');
+    return;
+  }
 
-    let guild = await util.getGuildConfig(message);
+  let guild = await util.getGuildConfig(message);
 
-    let moderation = '';
-    moderation += `Log: ${guild.logChannel ? `<#${guild.logChannel}>` : `disabled`} \n`;
-    moderation += `Muted role: ${guild.mutedRole ? `<@&${guild.mutedRole}>` : `disabled`} \n`;
-    moderation += `Mod roles: ${guild.modRoles.length ? `<@&${guild.modRoles.join('>, <@&')}>` : `none`} \n`;
+  let moderation = '';
+  moderation += `Log: ${guild.logChannel ? `<#${guild.logChannel}>` : `disabled`} \n`;
+  moderation += `Muted role: ${guild.mutedRole ? `<@&${guild.mutedRole}>` : `disabled`} \n`;
+  moderation += `Mod roles: ${guild.modRoles.length ? `<@&${guild.modRoles.join('>, <@&')}>` : `none`} \n`;
 
-    let support = '';
-    support += `Playlist: ${guild.playlist ? `https://www.youtube.com/playlist?list=${guild.playlist}` : `disabled`} \n`;
-    support += `Helpcenter: ${guild.helpcenter ? `https://${guild.helpcenter}.zendesk.com/` : `disabled`} \n`;
+  let support = '';
+  support += `Playlist: ${guild.playlist ? `https://www.youtube.com/playlist?list=${guild.playlist}` : `disabled`} \n`;
+  support += `Helpcenter: ${guild.helpcenter ? `https://${guild.helpcenter}.zendesk.com/` : `disabled`} \n`;
 
-    let automod = '';
-    automod += `Invites: ${guild.invites === false ? `forbidden` : `allowed`} \n`;
-    automod += `Link cooldown: ${guild.linkCooldown > 0 ? util.secToTime(guild.linkCooldown) : `disabled`} \n`;
+  let automod = '';
+  automod += `Invites: ${guild.invites === false ? `forbidden` : `allowed`} \n`;
+  automod += `Link cooldown: ${guild.linkCooldown > 0 ? util.secToTime(guild.linkCooldown) : `disabled`} \n`;
 
-    let embed = new Discord.MessageEmbed()
-    .setTitle(`Settings | Prefix: ${guild.prefix}`)
-    .addFields(
-      {name: `Moderation`, value: moderation, inline: false},
-      {name: `Support`, value: support, inline: false},
-      {name: `Automod`, value: automod, inline: false }
-    );
+  let embed = new Discord.MessageEmbed()
+  .setTitle(`Settings | Prefix: ${guild.prefix}`)
+  .addFields(
+    {name: `Moderation`, value: moderation, inline: false},
+    {name: `Support`, value: support, inline: false},
+    {name: `Automod`, value: automod, inline: false }
+  );
 
-    await message.channel.send(embed);
+  await message.channel.send(embed);
 
 };
 
