75cf56368188119074a4444222dab1fe88b68470
Julian <julianvennen@gmail.com>
2020-08-27 17:27:00 +0200

    log difference in message edits

--- features/messageUpdate/edited.js
+++ features/messageUpdate/edited.js
@@ -1,5 +1,6 @@
 const util = require('../../lib/util.js');
 const Discord = require('discord.js');
+const jsdiff = require('diff');
 
 exports.event = async (database, old, newMsg) => {
   if (old.author.bot) {
@@ -9,13 +10,27 @@ exports.event = async (database, old, newMsg) => {
     return;
   }
 
+  let diff = jsdiff.diffWords(old.content, newMsg.content);
+
+  let formatted = '';
+  for (let part of diff) {
+    if (part.added) {
+      formatted += `__${part.value}__`;
+    }
+    else if (part.removed) {
+      formatted += `~~${part.value}~~ `;
+    }
+    else {
+      formatted += part.value;
+    }
+  }
+
   let embed = new Discord.MessageEmbed()
     .setColor(util.color.orange)
     .setAuthor(`Message by ${old.author.username}#${old.author.discriminator} in #${old.channel.name} was edited`,old.author.avatarURL())
-    .addFields([
-      {name: 'Before:', value: old.content.substring(0,1024)},
-      {name: 'After:', value: newMsg.content.substring(0,1024)}
-    ])
+    .setDescription(
+      'Changes: ' + formatted.substring(0,1991)
+    )
     .setFooter(`ID: ${old.author.id}`);
 
   await util.logMessageEmbed(old, '', embed);
