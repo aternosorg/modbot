46f5a69ac1e24c422ec44a5b1cd9556431258b03
Julian <julianvennen@gmail.com>
2020-08-28 23:42:09 +0200

    unmute multi user

--- commands/unmute.js
+++ commands/unmute.js
@@ -14,55 +14,55 @@ command.execute = async (message, args, database, bot) => {
     return;
   }
 
-  let userId = util.userMentionToId(args.shift());
-  if (!userId) {
+  let users = await util.userMentions(args);
+
+  if (!users.length) {
     await message.channel.send(await util.usage(message, command.names[0]));
     return;
   }
 
-  let user;
-  try {
-    user = await bot.users.fetch(userId);
-  } catch (e) {
-    await message.react(util.icons.error);
-    await message.channel.send("User not found!");
-    return;
-  }
-  let member;
-  try {
-      member = await message.guild.members.fetch(userId);
-  } catch (e) {}
-  let guildConfig = await util.getGuildConfig(message);
-
-  if (user.bot) {
-    await message.react(util.icons.error);
-    await message.channel.send("You can't interact with bots!");
-    return;
-  }
-
-  if(!await database.query("SELECT * FROM moderations WHERE active = TRUE AND guildid = ? AND userid = ? AND action = 'mute'", [message.guild.id, userId]) && (member && !member.roles.cache.has(guildConfig.mutedRole))) {
-    await message.react(util.icons.error);
-    await message.channel.send("User isn't muted here!");
-    return;
-  }
-
   let reason = args.join(' ') || 'No reason provided.';
-  let now = Math.floor(Date.now()/1000);
 
-  if (member) {
-    await member.roles.remove([guildConfig.mutedRole], `${message.author.username}#${message.author.discriminator} | ` + reason);
-  }
-  await database.query("UPDATE moderations SET active = FALSE WHERE active = TRUE AND guildid = ? AND userid = ? AND action = 'mute'", [message.guild.id, userId]);
-  let insert = await database.queryAll("INSERT INTO moderations (guildid, userid, action, created, reason, moderator, active) VALUES (?,?,?,?,?,?,?)",[message.guild.id, userId,'unmute', now, reason, message.author.id, false]);
+  for(let userId of users) {
+    let user= await bot.users.fetch(userId);
 
-  if (member) {
+    let member;
     try {
-      await member.send(`You were unmuted in \`${message.guild.name}\` | ${reason}`);
+      member = await message.guild.members.fetch(userId);
     } catch (e) {}
+    let guildConfig = await util.getGuildConfig(message);
+
+    if (user.bot) {
+      await message.react(util.icons.error);
+      await message.channel.send("You can't interact with bots!");
+      continue;
+    }
+
+    if(!await database.query("SELECT * FROM moderations WHERE active = TRUE AND guildid = ? AND userid = ? AND action = 'mute'", [message.guild.id, userId]) && (member && !member.roles.cache.has(guildConfig.mutedRole))) {
+      await message.react(util.icons.error);
+      await message.channel.send(`<@${member.id}> isn't muted here!`);
+      continue;
+    }
+
+    let now = Math.floor(Date.now()/1000);
+
+    if (member) {
+      await member.roles.remove([guildConfig.mutedRole], `${message.author.username}#${message.author.discriminator} | ` + reason);
+    }
+    await database.query("UPDATE moderations SET active = FALSE WHERE active = TRUE AND guildid = ? AND userid = ? AND action = 'mute'", [message.guild.id, userId]);
+    let insert = await database.queryAll("INSERT INTO moderations (guildid, userid, action, created, reason, moderator, active) VALUES (?,?,?,?,?,?,?)",[message.guild.id, userId,'unmute', now, reason, message.author.id, false]);
+
+    if (member) {
+      try {
+        await member.send(`You were unmuted in \`${message.guild.name}\` | ${reason}`);
+      } catch (e) {}
+    }
+
+    await util.chatSuccess(message.channel, user, reason, "unmuted");
+    await util.logMessageModeration(message.guild.id, message.author, user, reason, insert.insertId, "Unmute");
+
   }
 
-  await util.chatSuccess(message.channel, user, reason, "unmuted");
-  await util.logMessageModeration(message.guild.id, message.author, user, reason, insert.insertId, "Unmute");
 };
 
 module.exports = command;
