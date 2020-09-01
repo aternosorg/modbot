9841f80cb2b74ade651d45d94cdd5532e01e8788
Julian <julianvennen@gmail.com>
2020-09-01 21:42:01 +0200

    …

--- commands/lock.js
+++ commands/lock.js
@@ -5,7 +5,7 @@ const command = {};
 
 command.description = 'Dissallow users to send messages to a channel';
 
-command.usage = '<global|#channel|id...> message';
+command.usage = '<global|#channel|id…> message';
 
 command.names = ['lock'];
 
--- commands/unlock.js
+++ commands/unlock.js
@@ -5,7 +5,7 @@ const command = {};
 
 command.description = 'Unlock a channel';
 
-command.usage = '<global|#channel|id...> message';
+command.usage = '<global|#channel|id…> message';
 
 command.names = ['unlock'];
 
