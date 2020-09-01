ded39b9e9ebd91767c891c771fd969a1a3fdf989
Julian <julianvennen@gmail.com>
2020-09-01 19:40:09 +0200

    global locking

--- commands/lock.js
+++ commands/lock.js
@@ -24,32 +24,29 @@ command.execute = async (message, args, database, bot) => {
     let updates = '';
     for(let channel of channels) {
       channel = message.guild.channels.resolve(channel);
-      let config = await util.getChannelConfig(channel.id);
-      let permissions = channel.permissionsFor(everyone);
 
-      let con = true;
-      for(const p of ['SEND_MESSAGES', 'ADD_REACTIONS']) {
-        if (permissions.has(p)) {
-          if (channel.permissionOverwrites.get(everyone)) {
-            config.lock[p] = channel.permissionOverwrites.get(everyone).allow.has(p) ? true : null;
-          }
-          else {
-            config.lock[p] = null;
-          }
-          let o = {};
-          o[p] = false;
-          await channel.updateOverwrite(everyone, o);
-          con = false;
-        }
-      }
-
-      if (con) {
+      if (await lock(channel, everyone, embed))
+        updates += `<#${channel.id}>, `;
+    }
+    if (updates.length) {
+      await message.channel.send(`Locked ${updates.substring(0,updates.length - 2)}!`);
+    }
+    else {
+      await message.channel.send(`No channels to lock!`);
+    }
+  }
+  else if (args.length && ['all','global'].includes(args[0])){
+    args = args.slice(1);
+    let embed = new Discord.MessageEmbed().setTitle('This channel has been locked!').setDescription(args.join(' ')).setColor(util.color.red);
+    channels = bot.guilds.cache.get(message.guild.id).channels.cache;
+    let updates = '';
+    for(let [id, channel] of channels) {
+      if (!(channel instanceof Discord.TextChannel)) {
         continue;
       }
 
-      await channel.send(embed);
-      await util.saveChannelConfig(config);
-      updates += `<#${channel.id}>, `;
+      if (await lock(channel, everyone, embed))
+        updates += `<#${channel.id}>, `;
     }
     if (updates.length) {
       await message.channel.send(`Locked ${updates.substring(0,updates.length - 2)}!`);
@@ -59,10 +56,38 @@ command.execute = async (message, args, database, bot) => {
     }
   }
   else {
-    //lock all channels with @e write
+    await message.channel.send(await util.usage(message, command.names[0]));
+  }
+};
+
+async function lock(channel, everyone, message) {
+  let config = await util.getChannelConfig(channel.id);
+  let permissions = channel.permissionsFor(everyone);
+
+  let con = true;
+  for(const p of ['SEND_MESSAGES', 'ADD_REACTIONS']) {
+    if (permissions.has(p)) {
+      if (channel.permissionOverwrites.get(everyone)) {
+        config.lock[p] = channel.permissionOverwrites.get(everyone).allow.has(p) ? true : null;
+      }
+      else {
+        config.lock[p] = null;
+      }
+      let o = {};
+      o[p] = false;
+      await channel.updateOverwrite(everyone, o);
+      con = false;
+    }
   }
 
+  if (con) {
+    return false;
+  }
 
-};
+  await channel.send(message);
+  await util.saveChannelConfig(config);
+
+  return true;
+}
 
 module.exports = command;
