385f94752cbdd228d091c6db404eb5329402b155
Julian <julianvennen@gmail.com>
2020-09-16 18:09:04 +0200

    allow removal of responses

--- commands/autoresponse.js
+++ commands/autoresponse.js
@@ -6,7 +6,7 @@ const command = {};
 
 command.description = 'Adds and lists auto-responses';
 
-command.usage = '<list|add|edit|remove>';
+command.usage = '<list|add|edit|remove> <id>';
 
 command.names = ['autoresponse','response'];
 
@@ -23,15 +23,15 @@ command.execute = async (message, args, database, bot) => {
     let guildConfig = await util.getGuildConfig(message);
     let responses = guildConfig.responses;
 
-    switch (args[0].toLowerCase()) {
+    switch (args.shift().toLowerCase()) {
         case 'list':
             if (!responses.length) {
                 return await message.channel.send("No auto-responses!");
             }
 
             let text = '';
-            for (const response of responses) {
-                text += `${response.trigger.type}: \`${response.trigger.content.replace('`','\`')}\`, `;
+            for (const [key, response] of responses.entries()) {
+                text += `[${key}] (${response.trigger.type}): \`${response.trigger.content.replace('`','\`')}\` \n`;
             }
             await message.channel.send(text.substring(0, 2000));
             break;
@@ -75,7 +75,7 @@ command.execute = async (message, args, database, bot) => {
             try {
                 options.response = (await message.channel.awaitMessages(response => {
                     return response.author.id === message.author.id
-                }, { max: 1, time: 15000, errors: ['time'] })).first().content;
+                }, { max: 1, time: 60000, errors: ['time'] })).first().content;
             }
             catch {
                 return await message.channel.send("You took to long to respond.");
@@ -86,7 +86,7 @@ command.execute = async (message, args, database, bot) => {
             try {
                 channels = (await message.channel.awaitMessages(async response => {
                     return response.author.id === message.author.id && (await util.channelMentions(message.guild,response.content.split(" "))).length || response.content.toLowerCase() === 'global'
-                }, { max: 1, time: 15000, errors: ['time'] })).first().content;
+                }, { max: 1, time: 60000, errors: ['time'] })).first().content;
             }
             catch {
                 return await message.channel.send("You took to long to respond.");
@@ -115,17 +115,52 @@ command.execute = async (message, args, database, bot) => {
             await message.channel.send(embed);
             break;
 
-        case 'edit':
-            await message.channel.send("WIP!");
-            break;
-
         case 'remove':
-            await message.channel.send("WIP!");
+            if (!args.length) {
+                await message.channel.send("Provide the id of the response you want to add");
+                return;
+            }
+            let id = parseInt(args.shift());
+            if (!responses[id]) {
+                await message.channel.send("Invalid id!");
+                return;
+            }
+            let response = responses[id];
+
+            let confirmation = await message.channel.send("Do you really want to delete this response?",new Discord.MessageEmbed()
+                .setTitle("Added new auto-response")
+                .setColor(util.color.green)
+                .addFields([
+                    {name: "Trigger", value: `${response.trigger.type}: ${response.trigger.content}`},
+                    {name: "Response", value: response.response.substring(0,1000)},
+                    {name: "Channels", value: response.global ? "global" : '<#' + response.channels.join('>, <#') + '>'}
+                ]));
+            {
+                let yes = confirmation.react(util.icons.yes);
+                let no = confirmation.react(util.icons.no);
+                await Promise.all([yes,no]);
+            }
+
+            let confirmed;
+            try {
+                confirmed = (await confirmation.awaitReactions((reaction, user) => {
+                    return user.id === message.author.id && (reaction.emoji.name === util.icons.yes || reaction.emoji.name === util.icons.no)
+                }, { max: 1, time: 15000, errors: ['time'] })).first().emoji.name === util.icons.yes;
+            }
+            catch {
+                return await message.channel.send("You took to long to react!");
+            }
+            if (!confirmed) {
+                return await message.channel.send("Cancelled!");
+            }
+            guildConfig.responses = responses.slice(0, id).concat(responses.slice(id + 1, responses.length));
+            await util.saveGuildConfig(guildConfig);
+            await message.channel.send("Removed!");
             break;
 
         default:
             return await message.channel.send(await util.usage(message,command.names[0]));
-    };
+    }
 };
 
 module.exports = command;
