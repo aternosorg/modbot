54f3bf81370ae7f435063d7ce7a56eda666232c1
Julian <julianvennen@gmail.com>
2020-10-10 18:29:58 +0200

    show channels in list

--- commands/autoresponse/list.js
+++ commands/autoresponse/list.js
@@ -11,11 +11,11 @@ module.exports = async (responses, message) => {
 
     let text = '';
     for (const [id, response] of responses) {
-        if(text.length > 1800){
+        if(text.length > 1500){
             await message.channel.send(text);
             text = '';
         }
-        text += `[${id}] (${response.trigger.type}): \`${response.trigger.content}\` \n`;
+        text += `[${id}] ${response.global ? "global" : response.channels.map(c => `<#${c}>`).join(', ')} (${response.trigger.type}): \`${response.trigger.content}\` \n`;
     }
     await message.channel.send(text.substring(0, 2000));
 };
