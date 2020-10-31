ebe9a5b18c3dd955e8454010bf0fe17d43fb7dc6
Julian <julianvennen@gmail.com>
2020-10-31 11:37:28 +0100

    dont trigger autoresponses on commands

--- src/features/message/respond.js
+++ src/features/message/respond.js
@@ -1,10 +1,8 @@
 const AutoResponse = require('../../AutoResponse');
+const commands = require('./commands');
 
 exports.event = async (options, message) => {
-  if (!message.guild || message.author.bot) {
-    return;
-  }
-
+  if (!message.guild || message.author.bot  || await commands.getCommand(message) !== null) return;
   const triggered = [];
 
   const responses = await AutoResponse.getAutoResponses(message.channel.id, message.guild.id);
