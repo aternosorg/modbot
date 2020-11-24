90f7fa9816fc536c4b761ba6b28c72768b1191d4
Julian <julian@aternos.org>
2020-11-24 19:06:54 +0100

    fix strike, fix bad word embed without punishments

--- src/BadWord.js
+++ src/BadWord.js
@@ -58,16 +58,19 @@ class BadWord extends ChatTriggeredFeature {
    * @returns {module:"discord.js".MessageEmbed}
    */
   embed(title, color) {
-    return new Discord.MessageEmbed()
+    const embed = new Discord.MessageEmbed()
         .setTitle(title + ` [${this.id}]`)
         .setColor(color)
         .addFields(
             /** @type {any} */[
           {name: "Trigger", value: `${this.trigger.type}: \`${this.trigger.type === 'regex' ? '/' + this.trigger.content + '/' + this.trigger.flags : this.trigger.content}\``},
-          {name: "Punishment", value: `${this.punishment.action} for ${this.punishment.duration}`},
           {name: "Response", value: this.response === 'default' ? BadWord.defaultResponse :this.response.substring(0,1000)},
           {name: "Channels", value: this.global ? "global" : this.channels.map(c => `<#${c}>`).join(', ')}
         ]);
+    if (this.punishment.action) {
+      embed.addField("Punishment", `${this.punishment.action} for ${this.punishment.duration}`)
+    }
+    return embed;
   }
 }
 
