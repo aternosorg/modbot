4fd125427afe2e8b7aa5e4810621f5a9fd569652
Julian <julian@aternos.org>
2020-11-25 15:18:30 +0100

    Revert "remove ip moderation"
    This reverts commit e3b1a7e2bc389c295c8b34cf62e92ccc268922f9.

---
+++ src/commands/ip.js
@@ -0,0 +1,73 @@
+const util = require('../util.js');
+
+const command = {};
+
+command.description = 'Require or forbid ips in a channel';
+
+command.usage = '#channel|channelId require|forbid|off';
+
+command.names = ['ip'];
+
+command.execute = async (message, args, database, bot) => {
+  //Permission check
+  if (!message.member.hasPermission('MANAGE_GUILD')) {
+    message.channel.send('You need the "Manage Server" permission to use this command.');
+    return;
+  }
+
+  //Get channel
+  let channelId = util.channelMentionToId(args.shift());
+  if (!message.guild.channels.cache.get(channelId)) {
+    await message.channel.send("Please specify a channel on this guild! (#mention) or ID");
+    return;
+  }
+
+  if(!args[0]){
+    message.channel.send("Subcommands: require, forbid, off");
+    return;
+  }
+
+  //convert sub command to mode
+  let subCommand = args.shift().toLowerCase();
+  let mode = getMode(subCommand);
+
+  if(mode === null){
+    message.channel.send("Subcommands: require, forbid, off");
+    return;
+  }
+
+  let channel = await util.getChannelConfig(channelId);
+  if (mode === 0) {
+    delete channel.mode;
+  }
+  else {
+    channel.mode = mode;
+  }
+  await util.saveChannelConfig(channel);
+  if (mode) {
+    await message.channel.send(`Set IPs to ${mode === 1 ? 'required' : 'forbidden'} in <#${channelId}>.`);
+  }
+  else {
+    await message.channel.send(`Disabled IP Moderation in <#${channelId}>!`);
+  }
+};
+
+/**
+ *
+ * @param string
+ * @return {Number|null}
+ */
+function getMode(string) {
+  if (['require','required'].includes(string)) {
+    return 1;
+  }
+  if (['forbid','forbidden'].includes(string)) {
+    return 2;
+  }
+  if (['off'].includes(string)) {
+    return 0;
+  }
+  return null;
+}
+
+module.exports = command;
---
+++ src/features/message/linkcooldownmod.js
@@ -0,0 +1,36 @@
+const util = require('../../util');
+const GuildConfig = require('../../GuildConfig');
+
+let users = {};
+
+exports.event = async (options, message) => {
+  if (!message.guild || await util.ignoresAutomod(message)) {
+    return;
+  }
+
+  if (!String(message.content).match(/https?:\/\//)) {
+    return;
+  }
+
+  let guild = await GuildConfig.get(message.guild.id);
+
+  if (!guild.linkCooldown) {
+    return;
+  }
+
+  let now = Math.floor(Date.now() / 1000);
+  if (users[message.author.id] && users[message.author.id] + guild.linkCooldown > now) {
+    await util.delete(message);
+    await util.logMessageDeletion(message, `link cooldown`);
+    let response = await message.channel.send(`<@!${message.author.id}> You can post a link again in ${util.secToTime(users[message.author.id] + guild.linkCooldown - now)}!`);
+    await util.delete(response, {timeout:3000});
+  }
+  else {
+    users[message.author.id] = now;
+    setTimeout(() => {
+      if (users[message.author.id]) {
+        delete users[message.author.id];
+      }
+    }, guild.linkCooldown * 1000);
+  }
+};
