d3a0ed9848320954e208701d8cd1e61eef847373
Bluewy <bluewyechache@gmail.com>
2020-12-06 14:38:50 +0100

    Better reason

--- src/features/message/badwordmod.js
+++ src/features/message/badwordmod.js
@@ -8,7 +8,7 @@ exports.event = async (options, message) => {
   const words = await BadWord.get(message.channel.id, message.guild.id);
   for (let [,word] of words) {
     if (word.matches(message)) {
-      const reason = `Using not allowed words/phrases`;
+      const reason = `Using forbidden words or phrases`;
       await util.delete(message, { reason: reason } );
       if (word.response !== 'disabled') {
         const response = await message.reply(word.response === 'default' ? BadWord.defaultResponse : word.response);
