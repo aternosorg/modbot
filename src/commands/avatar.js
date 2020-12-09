9cb4a8ad35749529f7ed47b8ce9cf06f266b998a
Julian <julian@aternos.org>
2020-12-09 16:26:56 +0100

    add avatar command

---
+++ src/commands/avatar.js
@@ -0,0 +1,24 @@
+const util = require('../util');
+const Discord = require('discord.js');
+
+const command = {};
+
+command.description = 'Search articles in the help center';
+
+command.usage = '<@user|id>';
+
+command.names = ['avatar','av'];
+
+command.execute = async (message, args, database, bot) => {
+    /** @type {module:"discord.js".User} */
+    const user = args.length ? await bot.users.fetch(util.userMentionToId(args[0])) : message.author;
+    const avatarEmbed = new Discord.MessageEmbed()
+        .setTitle(`Avatar of ${user.username}`)
+        .setImage(user.displayAvatarURL({dynamic: true, size: 512}))
+        .setFooter(`Command executed by ${message.author.username}`)
+        .setTimestamp();
+
+    await message.channel.send(avatarEmbed);
+};
+
+module.exports = command;
