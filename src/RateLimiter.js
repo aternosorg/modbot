87b15ceafd2e5ace24e4bf16ed092d24e6e3bdf4
Julian <julian@aternos.org>
2021-02-03 15:10:49 +0100

    increase limit to 5%, cache modcount

--- src/RateLimiter.js
+++ src/RateLimiter.js
@@ -1,3 +1,5 @@
+const Discord = require('discord.js');
+
 /**
  * Database
  * @type {Database}
@@ -17,6 +19,9 @@ class RateLimiter {
         database = db;
     }
 
+    static #modCountCache = new Discord.Collection();
+    static #modCountTimeouts = new Discord.Collection();
+
     /**
      * send a user a direct message
      * @param {module:"discord.js".Guild}                                   guild
@@ -25,15 +30,24 @@ class RateLimiter {
      * @return {Promise<void>}
      */
     static async sendDM(guild, user, message) {
-        let count = await database.query(`SELECT COUNT(*) AS count FROM moderations WHERE guildid = ${guild.id} AND created >= ${Math.floor(Date.now()/1000) - 60*60*24}`);
-        count = count.count;
+        let count = this.#modCountCache.get(guild.id);
+        if (!count) {
+            count = await database.query(`SELECT COUNT(*) AS count FROM moderations WHERE guildid = ${guild.id} AND created >= ${Math.floor(Date.now()/1000) - 60*60*24}`);
+            count = count.count;
+        }
 
-        if (count <= guild.memberCount * 0.01 || count <= 5) {
+        if (count <= guild.memberCount * 0.05 || count <= 5) {
             await user.send(message);
         }
         else {
             console.log(`Didn't send DM in guild ${guild.id}, count: ${count}`);
         }
+        count ++;
+        this.#modCountCache.set(guild.id, count);
+        if (this.#modCountTimeouts.has(guild.id)) clearTimeout(this.#modCountTimeouts.get(guild.id))
+        this.#modCountTimeouts.set(guild.id, setTimeout(() => {
+            this.#modCountCache.delete(guild.id);
+        }, 5*60*1000));
     }
 
 }
