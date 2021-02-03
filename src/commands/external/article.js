87282bf83e5fb0a3f16bcecdf5188e73f24ad93a
Julian <julian@aternos.org>
2021-02-03 12:56:53 +0100

    temp

--- src/commands/external/article.js
+++ src/commands/external/article.js
@@ -1,12 +1,35 @@
 const Command = require('../../Command');
+const axios = require('axios');
 
-class ExampleCommand extends Command {
+class ArticleCommand extends Command {
 
-    static names = ['example'];
+    static description = 'Search articles in the help center';
+
+    static usage = '<query>';
+
+    static names = ['article'];
 
     async execute() {
-        await this.message.channel.send("This is an example");
+        if (!this.guildConfig.helpcenter) {
+            await this.message.channel.send('No help center configured!');
+            return;
+        }
+
+        let query = this.args.join(' ').toLowerCase();
+        if(!query){
+            await this.sendUsage();
+        }
+
+
+        let response = await axios.get(`https://${this.guildConfig.helpcenter}.zendesk.com/api/v2/help_center/articles/search.json?query=`+encodeURIComponent(query));
+
+        if(response.data.results[0]){
+            await this.message.channel.send(response.data.results[0].html_url);
+        }
+        else {
+            await this.message.channel.send('No article found!');
+        }
     }
 }
 
-module.exports = ExampleCommand;
+module.exports = ArticleCommand;
