1657655dd2145511aefa87a2e5e10ed4f65390a7
Julian <julian@aternos.org>
2021-03-20 13:39:08 +0100

    refactor link cooldown command

---
+++ src/commands/settings/LinkCooldownCommand.js
@@ -0,0 +1,43 @@
+const Command = require('../../Command');
+const util = require('../../util')
+
+class LinkCooldownCommand extends Command {
+
+    static description = 'Configure link cooldown';
+
+    static usage = '<cooldown>|off|status';
+
+    static names = ['linkcooldown'];
+
+    static userPerms = ['MANAGE_GUILD'];
+
+    async execute() {
+        if (this.args.length < 1) {
+            await this.sendUsage();
+            return;
+        }
+
+        switch (this.args[0].toLowerCase()) {
+            case "off":
+                this.guildConfig.linkCooldown = -1;
+                await this.guildConfig.save();
+                await this.message.channel.send("Disabled link cooldown!")
+                break;
+            case "status":
+                await this.message.channel.send(`Link cooldown is currently ${this.guildConfig.linkCooldown === -1 ? 'disabled' : `set to ${util.secToTime(this.guildConfig.linkCooldown)}`}.`)
+                break;
+            default:
+                const cooldown = util.timeToSec(this.args.join(' '));
+                if (cooldown > 0) {
+                    this.guildConfig.linkCooldown = cooldown;
+                    await this.guildConfig.save();
+                    await this.message.channel.send(`Set link cooldown to ${util.secToTime(cooldown)}!`);
+                }
+                else {
+                    await this.sendUsage();
+                }
+        }
+    }
+}
+
+module.exports = LinkCooldownCommand;
