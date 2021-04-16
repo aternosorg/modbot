91ab044ae9aafde99e4abfd69e6807890bafa508
Julian <julian@aternos.org>
2021-04-16 16:09:21 +0200

    fix rateLimit, warning and error logging

--- src/features/warn/warningAlert.js
+++ src/features/warn/warningAlert.js
@@ -1,6 +1,6 @@
 const monitor = require('../../Monitor').getInstance();
 
-exports.event = async (warning) => {
+exports.event = async (options, warning) => {
     await monitor.error('The discord client emitted a warning', warning);
     console.error('The discord client emitted a warning', warning);
 }
