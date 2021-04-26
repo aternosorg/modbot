b70b2aaf793b2bc259c4d973e87656adcf90b09b
Julian <julian@aternos.org>
2021-04-26 22:24:06 +0200

    ignore quotes that never end

--- tests/util.test.js
+++ tests/util.test.js
@@ -24,3 +24,19 @@ test('split a string with double quotes in quotes', () => {
 test('split a string with an apostrophe', () => {
     expect(util.split("this isn't in quotes", " ")).toStrictEqual(["this", "isn't", "in", "quotes"]);
 })
+
+test('split a string with a single quote', () => {
+    expect(util.split("this 'quote never ends", " ")).toStrictEqual(["this", "'quote", "never", "ends"]);
+})
+
+test('split a string with a single quote at the end', () => {
+    expect(util.split("this quote never ends '", " ")).toStrictEqual(["this", "quote", "never", "ends", "'"]);
+})
+
+test('split a string with a single double quote', () => {
+    expect(util.split('this "quote never ends', " ")).toStrictEqual(["this", '"quote', "never", "ends"]);
+})
+
+test('split a string with a single double quote at the end', () => {
+    expect(util.split('this quote never ends "', " ")).toStrictEqual(["this", "quote", "never", "ends", '"']);
+})
