e83e341cbd3546ea0ea17480fa2561e3495dba98
Joshua <bluewyechache@gmail.com>
2021-06-06 22:15:20 +0200

    Set color to serverinfo embed

--- src/commands/utility/ServerInfoCommand.js
+++ src/commands/utility/ServerInfoCommand.js
@@ -29,6 +29,7 @@ class ServerInfoCommand extends Command{
 
         const embed = new Discord.MessageEmbed()
             .setTitle(`Info of ${guild.name}`)
+            .setColor(0xf04747)
             .setThumbnail(guild.iconURL({dynamic: true, size: 2048}))
             .setFooter(`Command executed by ${util.escapeFormatting(this.message.author.tag)}`)
             .setTimestamp()
