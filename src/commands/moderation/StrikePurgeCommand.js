0fc5df3f62ddadecc10212eeaa0905d94a1dbac6
Julian <julian@aternos.org>
2021-07-24 00:13:38 +0200

    add strike purge command

---
+++ src/commands/moderation/StrikePurgeCommand.js
@@ -0,0 +1,46 @@
+const ModerationCommand = require('../ModerationCommand');
+const Member = require('../../Member');
+const util = require('../../util');
+
+class StrikeCommand extends ModerationCommand {
+
+    static description = 'Strike a user and purge their messages';
+
+    static usage = '<@user|id> [<@user|id…>] [<count>] [<reason>]';
+
+    static names = ['strikepurge', 'sp'];
+
+    static userPerms = ['BAN_MEMBERS'];
+
+    static type = {
+        execute: 'strike',
+        done: 'striked',
+    };
+
+    async executePunishment(target) {
+        const member = new Member(target, this.message.guild);
+        await member.strike(this.database, this.reason, this.message.author, this.count);
+        return true;
+    }
+
+    async postPunishment(successes) {
+        let messages = await util.getMessages(this.message.channel, {
+            before: this.message.id,
+            limit: 100
+        });
+        messages = messages.filter(/** @type {module:"discord.js".Message} */msg => successes.some( u => msg.author.id === u.id));
+        await util.bulkDelete(this.message.channel, messages);
+    }
+
+    loadInfo() {
+        this.count = this.getCount();
+        super.loadInfo();
+    }
+
+    getCount() {
+        if (!this.args.length || !this.args[0].match(/^\d{1,5}$/)) return 1;
+        return parseInt(this.args.shift());
+    }
+}
+
+module.exports = StrikeCommand;
