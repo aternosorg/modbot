66233e1278aaaee841695a4b5b4f31ecb0cf891a
Julian <julian@aternos.org>
2021-08-09 22:35:23 +0200

    remove unused reason in deletion

--- src/features/messageCreate/badwordmod.js
+++ src/features/messageCreate/badwordmod.js
@@ -10,7 +10,7 @@ exports.event = async (options, message) => {
     for (let [,word] of words) {
         if (word.matches(message)) {
             const reason = 'Using forbidden words or phrases';
-            await util.delete(message, { reason: reason } );
+            await util.delete(message);
             if (word.response !== 'disabled') {
                 const response = await message.reply(word.response === 'default' ? BadWord.defaultResponse : word.response);
                 await util.delete(response, { timeout: 5000 });
