71fc91035ee0ca8f309b03edfc5c3ea3aa8c737f
Julian <julian@aternos.org>
2021-08-30 23:22:10 +0200

    update invite link to add slash command scope

--- src/commands/utility/InfoCommand.js
+++ src/commands/utility/InfoCommand.js
@@ -18,7 +18,7 @@ class InfoCommand extends Command {
             .setDescription(
                 '[ModBot](https://github.com/aternosorg/modbot/) is a moderation bot developed by [Aternos](https://aternos.org/)\n\n' +
                 '[[Privacy Policy]](https://aternos.gmbh/en/modbot/privacy) ' +
-                '[[Invite]](https://discordapp.com/oauth2/authorize?client_id=790967448111153153&scope=bot&permissions=268446806) '+
+                '[[Invite]](https://discordapp.com/oauth2/authorize?client_id=790967448111153153&scope=bot%20applications.commands&permissions=268446806) '+
                 '[[GitHub]](https://github.com/aternosorg/modbot/) ' +
                 '[[Discord]](https://discord.gg/zYYhgPtmxw) '
             )
