ce8a4facd4f8cb1656e6d113834dcf52e716cebe
Joshua <joshuaslui0203@gmail.com>
2021-09-26 20:51:12 +0200

    Update logJoin.js

--- src/features/guildMemberAdd/logJoin.js
+++ src/features/guildMemberAdd/logJoin.js
@@ -11,14 +11,12 @@ exports.event = async (options, member) => {
     let description = `**ID:** ${member.id}\n` +
         `**Created Account:** <t:${Math.floor(member.user.createdTimestamp / 1000)}:R>\n`;
     
-    const guild = member.guild;
-
     await Log.joinLog(member.guild.id, '', new MessageEmbed()
         .setTitle(`${member.user.tag} joined this server`)
         .setColor(util.color.green)
         .setThumbnail(member.user.avatarURL())
         .setDescription(description)
         .setTimestamp()
-        .setFooter(`Now at ${guild.memberCount} members`)
+        .setFooter(`Now at ${member.guild.memberCount} members`)
     );
 };
