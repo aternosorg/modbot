2d4388b103e4bdab6b64cd7b2f79ec0497afa93a
Julian <julian@aternos.org>
2021-10-29 12:38:18 +0200

    use old image rule

--- src/commands/utility/ArticleCommand.js
+++ src/commands/utility/ArticleCommand.js
@@ -20,15 +20,15 @@ class ArticleCommand extends Command {
         }
 
         const query = this.options.getString('query');
-        if(!query){
+        if (!query) {
             await this.sendUsage();
             return;
         }
 
-        const request = new Request(`https://${this.guildConfig.helpcenter}.zendesk.com/api/v2/help_center/articles/search.json?query=`+encodeURIComponent(query));
+        const request = new Request(`https://${this.guildConfig.helpcenter}.zendesk.com/api/v2/help_center/articles/search.json?query=` + encodeURIComponent(query));
         await request.getJSON();
 
-        if(request.JSON.count !== 0){
+        if (request.JSON.count !== 0) {
             /**
              * @type {{html_url: String, title: String, label_names: String[], body: String}}
              */
@@ -56,8 +56,7 @@ class ArticleCommand extends Command {
             }
 
             await this.reply(options);
-        }
-        else {
+        } else {
             await this.sendError('No article found!');
         }
     }
@@ -93,27 +92,30 @@ class ArticleCommand extends Command {
         //set up turndown
         const turndown = new Turndown({
             bulletListMarker: '-'
-        });
-        //convert headings to bold
-        turndown.addRule('headings', {
-            filter: ['h1','h2','h3','h4','h5','h6'],
-            replacement: function (content) {
-                if (!content) return '';
-                return '**' + content.replaceAll('**', '') + '**\n';
-            }
-        });
-        //ignore pre tags
-        turndown.addRule('codeblocks', {
-            filter: ['pre'],
-            replacement: function (content) {
-                return '```' + content.replace(/(?<!\\)[*_~`]+/g, '') + '```';
-            }
-        });
+        })
+            //convert headings to bold
+            .addRule('headings', {
+                filter: ['h1', 'h2', 'h3', 'h4', 'h5', 'h6'],
+                replacement: function (content) {
+                    if (!content) return '';
+                    return '**' + content.replaceAll('**', '') + '**\n';
+                }
+            })
+            //ignore pre tags
+            .addRule('codeblocks', {
+                filter: ['pre'],
+                replacement: function (content) {
+                    return '```' + content.replace(/(?<!\\)[*_~`]+/g, '') + '```';
+                }
+            })
+            //remove img tags
+            .addRule('images', {
+                filter: ['img'],
+                replacement: function () {
+                    return '';
 
-        //remove existing image rule
-        turndown.rules.array = turndown.rules.array.filter(r => !(r.filter === 'img' || (Array.isArray(r.filter) && r.filter.includes('img'))));
-        turndown.remove('img');
-
+                }
+            });
         //convert string
         let string = turndown.turndown(result.body);
         if (string.length > 800) {
