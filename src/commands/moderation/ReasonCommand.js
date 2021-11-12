6cb7106e3f1dbf4e636a8f4155017b18a2d3814d
Julian <julian@aternos.org>
2021-11-12 15:06:07 +0100

    /reason

--- src/commands/moderation/ReasonCommand.js
+++ src/commands/moderation/ReasonCommand.js
@@ -12,14 +12,47 @@ class ReasonCommand extends Command {
 
     static modCommand = true;
 
+    static supportsSlashCommands = true;
+
     async execute() {
-        const id = parseInt(this.args.shift());
-        if (!this.args.length || !id) return this.sendUsage();
+        const id = this.options.getNumber('id');
+        const reason = this.options.getString('reason').substring(0, 1000);
+        if (id == null || !reason) {
+            return this.sendUsage();
+        }
 
-        const reason = this.args.join(' ').substring(0, 1000);
-
-        const { affectedRows } = await this.database.queryAll('UPDATE moderations SET reason = ? WHERE id = ? AND guildid = ?', [reason, id, this.message.guild.id]);
+        const { affectedRows } = await this.database.queryAll('UPDATE moderations SET reason = ? WHERE id = ? AND guildid = ?', [reason, id, this.source.getGuild().id]);
         await this.reply(affectedRows ? `Updated reason of ${id} | ${reason}` : 'Moderation not found!');
+    }
+
+    static getOptions() {
+        return [{
+            name: 'id',
+            type: 'NUMBER',
+            description: 'Moderation ID',
+            required: true,
+        },{
+            name: 'reason',
+            type: 'STRING',
+            description: 'Updated moderation reason',
+            required: true,
+        }];
+    }
+
+    parseOptions(args) {
+        const id = args.shift()?.match(/#?(\d+)/);
+        return [
+            {
+                name: 'id',
+                type: 'NUMBER',
+                value: id ? parseInt(id[1]) : id,
+            },
+            {
+                name: 'reason',
+                type: 'STRING',
+                value: args.join(' '),
+            }
+        ];
     }
 }
 
