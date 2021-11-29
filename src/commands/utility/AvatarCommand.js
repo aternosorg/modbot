c5f672644ea786dcc464f9f7eb0d449fb094e01d
Julian <julian@aternos.org>
2021-11-29 15:48:32 +0100

    fix avatar command

--- src/commands/utility/AvatarCommand.js
+++ src/commands/utility/AvatarCommand.js
@@ -22,7 +22,7 @@ class AvatarCommand extends Command{
 
         const avatarEmbed = new MessageEmbed()
             .setTitle(`Avatar of ${util.escapeFormatting(user.tag)}`)
-            .setImage(user.displayAvatarURL({dynamic: true, size: 2048, format: png}))
+            .setImage(user.displayAvatarURL({dynamic: true, size: 2048, format: 'png'}))
             .setFooter(`Command executed by ${util.escapeFormatting(this.source.getUser().tag)}`)
             .setTimestamp();
 
