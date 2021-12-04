88b04b3b1ee954b482a3d05a909abf2cc5228553
Julian <julian@aternos.org>
2021-12-04 22:59:20 +0100

    remove autoresponse command

--- src/commands/settings/AutoResponseCommand.js
+++ src/commands/settings/AutoResponseCommand.js
@@ -145,6 +145,50 @@ class AddAutoResponseCommand extends SubCommand {
     }
 }
 
+class RemoveAutoResponseCommand extends SubCommand {
+
+    static names = ['remove'];
+
+    static description = 'Remove an auto-response.';
+
+    static usage = '<id>';
+
+    async execute() {
+        const id = this.options.getInteger('id');
+        if (!id || id < 0) {
+            return this.sendUsage();
+        }
+        /**
+         * @type {AutoResponse}
+         */
+        const response = await AutoResponse.getByID(id);
+        if (!response) {
+            return this.sendUsage();
+        }
+        await response.remove();
+        await this.reply(response.embed('Removed auto-response', util.color.red));
+    }
+
+
+    static getOptions() {
+        return [{
+            name: 'id',
+            type: 'INTEGER',
+            description: 'The ID of the auto-response that should be removed.',
+            required: true,
+            minValue: 0,
+        }];
+    }
+
+    parseOptions(args) {
+        return [{
+            name: 'id',
+            type: 'INTEGER',
+            value: parseInt(args.shift()),
+        }];
+    }
+}
+
 class AutoResponseCommand extends ConfigCommand {
 
     static description = 'Manage auto-responses';
@@ -153,10 +197,10 @@ class AutoResponseCommand extends ConfigCommand {
 
     static userPerms = ['MANAGE_GUILD'];
 
-    static usage = 'list|add';
+    static usage = 'list|add|remove';
 
     static getSubCommands() {
-        return [ListAutoResponseCommand, AddAutoResponseCommand];
+        return [ListAutoResponseCommand, AddAutoResponseCommand, RemoveAutoResponseCommand];
     }
 }
 
