7467b7dec8c8dfefdd5a99b4a849d31388e4b5e1
Julian <julian@aternos.org>
2021-12-14 19:36:30 +0100

    increase timeout

--- src/features/messageCreate/badwordmod.js
+++ src/features/messageCreate/badwordmod.js
@@ -22,7 +22,7 @@ exports.event = async (options, /** @type {Message} */message) => {
             headers: {
                 'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/95.0.4638.69 Safari/537.36'
             },
-            timeout: 3000
+            timeout: 60000
         });
 
         try {
