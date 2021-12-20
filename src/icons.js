8f7d0689c3ef34aecdebb96806605087d96c24a1
Amgelo563 <luisangeloarce314@gmail.com>
2021-12-20 13:17:59 -0500

    Restore left and right icons on icons.js

--- src/icons.js
+++ src/icons.js
@@ -6,5 +6,7 @@ module.exports = {
     forbidden: String.fromCodePoint(9940),
     no: String.fromCodePoint(10060),
     yes: String.fromCodePoint(9989),
+    left: String.fromCodePoint(11013),
+    right: String.fromCodePoint(10145),
     eyes: String.fromCodePoint(128064),
 };
