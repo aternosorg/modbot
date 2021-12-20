f563af95e7bf5ead2f3e1f7b309b1992adadb0f6
Julian <julian@aternos.org>
2021-12-20 09:18:11 +0100

    /linkcooldown

--- src/commands/settings/LinkCooldownCommand.js
+++ src/commands/settings/LinkCooldownCommand.js
@@ -1,42 +1,24 @@
-const Command = require('../Command');
-const util = require('../../util');
+const ConfigCommand = require('../ConfigCommand');
+const DisableLinkCooldownCommand = require('./linkcooldown/DisableLinkCooldownCommand');
+const GetLinkCooldownCommand = require('./linkcooldown/GetLinkCooldownCommand');
+const SetLinkCooldownCommand = require('./linkcooldown/SetLinkCooldownCommand');
 
-class LinkCooldownCommand extends Command {
+class LinkCooldownCommand extends ConfigCommand {
 
     static description = 'Configure link cooldown';
 
-    static usage = '<cooldown>|off|status';
+    static usage = 'set|off|status';
 
     static names = ['linkcooldown'];
 
     static userPerms = ['MANAGE_GUILD'];
 
-    async execute() {
-        if (this.args.length < 1) {
-            await this.sendUsage();
-            return;
-        }
-
-        switch (this.args[0].toLowerCase()) {
-            case 'off':
-                this.guildConfig.linkCooldown = -1;
-                await this.guildConfig.save();
-                await this.reply('Disabled link cooldown!');
-                break;
-            case 'status':
-                await this.reply(`Link cooldown is currently ${this.guildConfig.linkCooldown === -1 ? 'disabled' : `set to ${util.secToTime(this.guildConfig.linkCooldown)}`}.`);
-                break;
-            default: {
-                const cooldown = util.timeToSec(this.args.join(' '));
-                if (cooldown > 0) {
-                    this.guildConfig.linkCooldown = cooldown;
-                    await this.guildConfig.save();
-                    await this.reply(`Set link cooldown to ${util.secToTime(cooldown)}!`);
-                } else {
-                    await this.sendUsage();
-                }
-            }
-        }
+    static getSubCommands() {
+        return [
+            DisableLinkCooldownCommand,
+            GetLinkCooldownCommand,
+            SetLinkCooldownCommand,
+        ];
     }
 }
 
