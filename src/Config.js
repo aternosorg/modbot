fdb9a1989c522eb3ac75cb755de7a2cc3515b03e
Julian <julian@aternos.org>
2022-09-02 16:07:10 +0200

    add config with option to load from env

---
+++ src/Config.js
@@ -0,0 +1,101 @@
+import Logger from './logging/Logger.js';
+import {exists, readJSON} from './util/fsutils.js';
+
+/**
+ * @typedef {Object} ConfigData
+ * @property {string} authToken
+ * @property {DatabaseConfig} database
+ * @property {?string} googleApiKey
+ * @property {?MonitoringConfig} monitoring
+ * @property {{enabled: boolean, guild: string}} debug
+ * @property {string[]} featureWhitelist
+ */
+
+/**
+ * @typedef {Object} DatabaseConfig
+ */
+
+/**
+ * @typedef {Object} MonitoringConfig google cloud monitoring
+ * @property {boolean} enabled
+ * @property {string} projectId
+ * @property {string} logName
+ * @property {MonitoringCredentials} credentials
+ */
+
+/**
+ * @typedef {Object} MonitoringCredentials
+ * @property {string} client_email
+ * @property {string} private_key
+ */
+
+export default class Config {
+    static #instance = new Config();
+
+    /**
+     * @type {ConfigData}
+     */
+    #data;
+
+    /**
+     * @returns {Config}
+     */
+    static get instance() {
+        return this.#instance;
+    }
+
+    get data() {
+        return this.#data;
+    }
+
+    async load() {
+        if (process.env.MODBOT_USE_ENV) {
+            // load config from env
+            this.#data = {
+                authToken: process.env.MODBOT_AUTH_TOKEN,
+                database: {
+                    host: process.env.MODBOT_DATABASE_HOST,
+                    user: process.env.MODBOT_DATABASE_USER,
+                    password: process.env.MODBOT_DATABASE_PASSWORD,
+                    database: process.env.MODBOT_DATABASE_DATABASE,
+                    port: parseInt(process.env.MODBOT_DATABASE_PORT ?? '3306'),
+                },
+                googleApiKey:  process.env.MODBOT_GOOGLE_API_KEY,
+                monitoring: {
+                    enabled: !['0', 'false'].includes(process.env.MODBOT_MONITORING_ENABLED),
+                    projectId: process.env.MODBOT_MONITORING_PROJECT_ID,
+                    logName: process.env.MODBOT_MONITORING_LOG_NAME,
+                    credentials: {
+                        client_email: process.env.MODBOT_MONITORING_CREDENTIALS_CLIENT_EMAIL,
+                        private_key: process.env.MODBOT_MONITORING_CREDENTIALS_PRIVATE_KEY
+                    }
+                },
+                debug: {
+                    enabled: !['0', 'false'].includes(process.env.MODBOT_DEBUG_ENABLED),
+                    guild: process.env.MODBOT_DEBUG_GUILD
+                },
+                featureWhitelist: process.env.MODBOT_DEBUG_GUILD.split(/ *, */),
+            };
+        }
+        else {
+            // load config from file
+            if (!await exists('./config.json')) {
+                await Logger.instance.error('No config file found.\n' +
+                    'Create a config.json or use environment variables as described in the README.md');
+                // TODO
+                process.exit(1);
+            }
+
+            this.#data = await readJSON('./config.json');
+            this.#data.monitoring ??= {
+                enabled: false,
+                projectId: '',
+                logName: '',
+                credentials: {
+                    client_email: '',
+                    private_key: '',
+                },
+            };
+        }
+    }
+}
---
+++ src/util/fsutils.js
@@ -0,0 +1,28 @@
+import * as fs from 'fs/promises';
+import {readFile} from 'fs/promises';
+
+/**
+ * @param {string} path
+ * @return {Promise<boolean>}
+ */
+export async function exists(path) {
+    try {
+        await fs.stat(path);
+    }
+    catch (e) {
+        if (e.code === 'ENOENT') {
+            return false;
+        }
+        throw e;
+    }
+    return true;
+}
+
+/**
+ * read a json file
+ * @param {string} path
+ * @return {Promise<*>}
+ */
+export async function readJSON(path) {
+    return JSON.parse(readFile(path).toString());
+}
