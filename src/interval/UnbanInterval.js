2135cad1e4462ec70e9cfc022777011cd934bced
Julian <julian@aternos.org>
2022-09-02 23:20:14 +0200

    use better log class to log unbans

--- src/interval/UnbanInterval.js
+++ src/interval/UnbanInterval.js
@@ -1,9 +1,11 @@
 import Interval from './Interval.js';
 import Database from '../bot/Database.js';
-import Log from '../discord/GuildLog.js';
 import Bot from '../bot/Bot.js';
-import {RESTJSONErrorCodes} from 'discord.js';
+import {MessageEmbed, RESTJSONErrorCodes} from 'discord.js';
 import Logger from '../logging/Logger.js';
+import GuildWrapper from '../discord/GuildWrapper.js';
+import util from '../util.js';
+import MemberWrapper from '../discord/MemberWrapper.js';
 
 export default class UnbanInterval extends Interval {
     getInterval() {
@@ -16,11 +18,24 @@ export default class UnbanInterval extends Interval {
         for (const result of await Database.instance.queryAll('SELECT * FROM moderations WHERE action = \'ban\' AND active = TRUE AND expireTime IS NOT NULL AND expireTime <= ?',
             Math.floor(Date.now()/1000))) {
             const user = await client.users.fetch(result.userid);
+            /** @property {number} insertId */
             const unban = await Database.instance.queryAll('INSERT INTO moderations (guildid, userid, action, created, reason, active) VALUES (?,?,?,?,?,?)', result.guildid, result.userid, 'unban', Math.floor(Date.now()/1000), reason, false);
+            const guild = await GuildWrapper.fetch(result.guildid);
+            const member = new MemberWrapper(user, guild);
+
             await Database.instance.query('UPDATE moderations SET active = FALSE WHERE action = \'ban\' AND userid = ? AND guildid = ?',result.userid, result.guildid);
             try {
-                await client.guilds.resolve(result.guildid).members.unban(result.userid, reason);
-                await Log.logCheck(result.guildid, user, reason, unban.insertId, 'Unban');
+                await member.unban(reason);
+                const embed = new MessageEmbed()
+                    .setColor(util.color.green)
+                    .setAuthor({name: `Case ${unban.insertId} | Unban | ${user.tag}`, iconURL: user.avatarURL()})
+                    .setFooter({text: user.id})
+                    .setTimestamp()
+                    .addFields(
+                        /** @type {any} */ { name: 'User', value: `<@!${user.id}>`, inline: true},
+                        /** @type {any} */ { name: 'Reason', value: reason.substring(0, 512), inline: true}
+                    );
+                await guild.log({embeds: [embed]});
             }
             catch (e) {
                 if (![RESTJSONErrorCodes.UnknownBan].includes(e.code)) {
