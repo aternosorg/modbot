ef84380154223d5ac8392490fe2e72ef9f17a8c1
Julian <julian@aternos.org>
2022-09-05 19:03:55 +0200

    base interaction create listener

---
+++ src/events/interactionCreate/InteractionCreateEventListener.js
@@ -0,0 +1,16 @@
+import EventListener from '../EventListener.js';
+
+export default class InteractionCreateEventListener extends EventListener {
+    get name() {
+        return 'interactionCreate';
+    }
+
+    /**
+     * @abstract
+     * @param {import('discord.js').BaseInteraction} interaction
+     * @return {Promise<unknown>}
+     */
+    async execute(interaction) {
+        interaction.createdAt;
+    }
+}
