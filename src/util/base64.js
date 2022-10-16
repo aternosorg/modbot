0deb71accb2c66028d246feabbf2ce1005469a8c
Julian <julian@aternos.org>
2022-10-16 23:12:25 +0200

    Remove unused util file

--- src/util/base64.js
+++
@@ -1,7 +0,0 @@
-export function encode64(string) {
-    return new Buffer.from(string).toString('base64');
-}
-
-export function decode64(string) {
-    return new Buffer.from(string, 'base64').toString('utf-8');
-}
