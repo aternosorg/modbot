b127557e85b9d8a9138fb8b0656fb3beb3081a3e
Julian <julian@aternos.org>
2023-01-26 14:51:30 +0100

    Send individual embeds as new messages

--- src/commands/settings/bad-word/ListBadWordCommand.js
+++ src/commands/settings/bad-word/ListBadWordCommand.js
@@ -1,17 +1,20 @@
-import {MESSAGE_EMBED_LIMIT} from '../../../util/apiLimits.js';
-import LineEmbed from '../../../embeds/LineEmbed.js';
 import SubCommand from '../../SubCommand.js';
 import BadWord from '../../../database/BadWord.js';
 
 export default class ListBadWordCommand extends SubCommand {
 
     async execute(interaction) {
-        const messages = await BadWord.getGuildOverview(interaction.guild, 'Bad-word')
-            ?? new LineEmbed();
-        await interaction.reply({
-            ephemeral: true,
-            embeds: messages.slice(0, MESSAGE_EMBED_LIMIT)
-        });
+        const embeds = await BadWord.getGuildOverview(interaction.guild, 'Bad-word');
+
+        for (const [index, embed] of embeds.entries()) {
+            const options = { ephemeral: true, embeds: [embed] };
+            if (index === 0) {
+                await interaction.reply(options);
+            }
+            else {
+                await interaction.followUp(options);
+            }
+        }
     }
 
     getDescription() {
