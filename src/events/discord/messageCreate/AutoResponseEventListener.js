96e3bb075eb73369f2f16cb01f365025a3fc3119
Tylad <93615331+TyladYT@users.noreply.github.com>
2023-10-25 23:56:02 +0100

    Add a cooldown for tags (per-channel)
    This modification adds a cooldown per auto-response (tags) per-channel to make sure that if multiple people execute a tag all at once, then the bot doesn't send it twice. It'll only respond to one tag.

--- src/events/discord/messageCreate/AutoResponseEventListener.js
+++ src/events/discord/messageCreate/AutoResponseEventListener.js
@@ -1,6 +1,8 @@
 import MessageCreateEventListener from './MessageCreateEventListener.js';
 import AutoResponse from '../../../database/AutoResponse.js';
 import {ThreadChannel} from 'discord.js';
+const recentAutoresponse = new Set(); // creates a new Set for cooldowns
+
 
 export default class AutoResponseEventListener extends MessageCreateEventListener {
 
@@ -18,9 +20,19 @@ export default class AutoResponseEventListener extends MessageCreateEventListene
         const responses = (await AutoResponse.get(channel.id, message.guild.id)).values();
         const triggered = Array.from(responses).filter(response => response.matches(message));
 
+		let tagCooldown = 10000 //10 seconds, e.g. 5000 = 5 seconds
+        
         if (triggered.length) {
             const response = triggered[Math.floor(Math.random() * triggered.length)];
-            await message.reply({content: response.response});
+            if (recentAutoresponse.has(message.channel.id)) { // checks if a tag has been used in this channel in the last <cooldown time>
+            	message.react('⏲️') // react with timer emoji to show that there is currently a cooldown active
+    		} else {
+				await message.reply({content: response.response});
+        		recentAutoresponse.add(message.channel.id);
+        		setTimeout(() => {
+          			recentAutoresponse.delete(message.channel.id); // removes the cooldown after <cooldown time>
+        		}, tagCooldown);
+    		}
         }
     }
 }
